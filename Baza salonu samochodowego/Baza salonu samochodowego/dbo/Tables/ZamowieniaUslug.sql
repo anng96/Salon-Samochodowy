f91db6e0dded2a05c8d98efba0e63b6b7d8ef57e
anng96 <43572687+anng96@users.noreply.github.com>
2019-02-08 00:02:28 +0100

    tabela zamowienia z obszaru serwis

---
+++ Baza salonu samochodowego/Baza salonu samochodowego/dbo/Tables/ZamowieniaUslug.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[ZamowieniaUslug] (
+    [ZamowienieIdU]  INT   IDENTITY (1, 1) NOT NULL,
+    [ModelId]        INT   NOT NULL,
+    [UslugaId]       INT   NOT NULL,
+    [CenaZamowienia] MONEY NOT NULL,
+    [DataZamowienia] DATE  NOT NULL,
+    CONSTRAINT [PK_ZamowieniaUslug] PRIMARY KEY CLUSTERED ([ZamowienieIdU] ASC),
+    CONSTRAINT [FK_ZamowieniaUslug_Modele] FOREIGN KEY ([ModelId]) REFERENCES [dbo].[Modele] ([ModelId]),
+    CONSTRAINT [FK_ZamowieniaUslug_Uslugi] FOREIGN KEY ([UslugaId]) REFERENCES [dbo].[Uslugi] ([UslugaId])
+);
+
