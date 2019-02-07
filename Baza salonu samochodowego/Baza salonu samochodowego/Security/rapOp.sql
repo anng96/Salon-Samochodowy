df217147fb18b427775cb98375b11990acf157b2
anng96 <43572687+anng96@users.noreply.github.com>
2019-02-08 00:32:20 +0100

    Utworzenie schematu

---
+++ Baza salonu samochodowego/Baza salonu samochodowego/Security/rapOp.sql
@@ -0,0 +1,3 @@
+﻿CREATE SCHEMA [rapOp]
+    AUTHORIZATION [db_owner];
+
---
+++ Baza salonu samochodowego/Baza salonu samochodowego/dbo/Tables/HistoriaZamowienia.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[HistoriaZamowienia] (
+    [ZamowienieId]   INT  IDENTITY (1, 1) NOT NULL,
+    [Status]         INT  NOT NULL,
+    [PracownikId]    INT  NOT NULL,
+    [DataZamowienia] DATE DEFAULT (getdate()) NULL,
+    CONSTRAINT [PK_HistoriaZamowienia] PRIMARY KEY CLUSTERED ([ZamowienieId] ASC),
+    CONSTRAINT [FK_HistoriaZamowienia_Pracownicy] FOREIGN KEY ([PracownikId]) REFERENCES [dbo].[Pracownicy] ([PracownikId]),
+    CONSTRAINT [FK_HistoriaZamowienia_Status] FOREIGN KEY ([Status]) REFERENCES [dbo].[StatusZamowienia] ([StatusId]),
+    CONSTRAINT [FK_HistoriaZamowienia_ZamowieniaUslug] FOREIGN KEY ([ZamowienieId]) REFERENCES [dbo].[ZamowieniaUslug] ([ZamowienieIdU])
+);
+
