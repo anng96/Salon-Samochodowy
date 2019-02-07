52ccdd37a905d98a5e58e941dc1bb68ac216704e
anng96 <43572687+anng96@users.noreply.github.com>
2019-02-07 23:24:46 +0100

    Dodanie tabeli m2m

---
+++ Baza salonu samochodowego/Baza salonu samochodowego/dbo/Tables/PracownikZespol.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[PracownikZespol] (
+    [PracownikId] INT NOT NULL,
+    [ZespolId]    INT NOT NULL,
+    CONSTRAINT [PK_PracownikZespol] PRIMARY KEY CLUSTERED ([PracownikId] ASC, [ZespolId] ASC),
+    CONSTRAINT [FK_PracownikZespol] FOREIGN KEY ([ZespolId]) REFERENCES [dbo].[Zespoly] ([ZespolId]),
+    CONSTRAINT [FK_PracownikZespol_Pracownicy] FOREIGN KEY ([PracownikId]) REFERENCES [dbo].[Pracownicy] ([PracownikId])
+);
+
