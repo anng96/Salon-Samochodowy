1e2050a283f19b75c1910064fd0b6088b78e80e5
anng96 <43572687+anng96@users.noreply.github.com>
2019-02-07 22:49:11 +0100

    Dodanie tabel do obszaru HR

---
+++ Baza salonu samochodowego/Baza salonu samochodowego/dbo/Tables/Stanowiska.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Stanowiska] (
+    [StanowiskoId]    INT           IDENTITY (1, 1) NOT NULL,
+    [NazwaStanowiska] VARCHAR (100) NULL,
+    PRIMARY KEY CLUSTERED ([StanowiskoId] ASC),
+    UNIQUE NONCLUSTERED ([NazwaStanowiska] ASC)
+);
+
---
+++ Baza salonu samochodowego/Baza salonu samochodowego/dbo/Tables/Zespoly.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Zespoly] (
+    [ZespolId]     INT           IDENTITY (1, 1) NOT NULL,
+    [NazwaZespolu] VARCHAR (100) NULL,
+    PRIMARY KEY CLUSTERED ([ZespolId] ASC),
+    UNIQUE NONCLUSTERED ([NazwaZespolu] ASC)
+);
+
