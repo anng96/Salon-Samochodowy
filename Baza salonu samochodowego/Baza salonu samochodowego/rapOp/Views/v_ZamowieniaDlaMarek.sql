f3f8c9d33beaa1117b1fde1f179ed780b32036be
anng96 <43572687+anng96@users.noreply.github.com>
2019-02-08 21:03:51 +0100

    Utworzenie widoków

---
+++ Baza salonu samochodowego/Baza salonu samochodowego/rapOp/Views/v_ZamowieniaDlaMarek.sql
@@ -0,0 +1,141 @@
+﻿CREATE VIEW rapOp.v_ZamowieniaDlaMarek
+AS
+SELECT        m.Nazwa, COUNT(z.ZamowienieId) AS IleZamowienMarek, SUM(z.Cena) AS KwotaZamowien
+FROM            dbo.Marki AS m WITH (NOLOCK) LEFT OUTER JOIN
+                         dbo.Modele AS mo ON m.MarkaId = mo.MarkaId LEFT OUTER JOIN
+                         dbo.Zamowienia AS z ON mo.ModelId = z.ModelId
+GROUP BY m.Nazwa
+GO
+EXECUTE sp_addextendedproperty @name = N'MS_DiagramPaneCount', @value = 1, @level0type = N'SCHEMA', @level0name = N'rapOp', @level1type = N'VIEW', @level1name = N'v_ZamowieniaDlaMarek';
+
+
+GO
+EXECUTE sp_addextendedproperty @name = N'MS_DiagramPane1', @value = N'[0E232FF0-B466-11cf-A24F-00AA00A3EFFF, 1.00]
+Begin DesignProperties = 
+   Begin PaneConfigurations = 
+      Begin PaneConfiguration = 0
+         NumPanes = 4
+         Configuration = "(H (1[40] 4[20] 2[20] 3) )"
+      End
+      Begin PaneConfiguration = 1
+         NumPanes = 3
+         Configuration = "(H (1 [50] 4 [25] 3))"
+      End
+      Begin PaneConfiguration = 2
+         NumPanes = 3
+         Configuration = "(H (1 [50] 2 [25] 3))"
+      End
+      Begin PaneConfiguration = 3
+         NumPanes = 3
+         Configuration = "(H (4 [30] 2 [40] 3))"
+      End
+      Begin PaneConfiguration = 4
+         NumPanes = 2
+         Configuration = "(H (1 [56] 3))"
+      End
+      Begin PaneConfiguration = 5
+         NumPanes = 2
+         Configuration = "(H (2 [66] 3))"
+      End
+      Begin PaneConfiguration = 6
+         NumPanes = 2
+         Configuration = "(H (4 [50] 3))"
+      End
+      Begin PaneConfiguration = 7
+         NumPanes = 1
+         Configuration = "(V (3))"
+      End
+      Begin PaneConfiguration = 8
+         NumPanes = 3
+         Configuration = "(H (1[56] 4[18] 2) )"
+      End
+      Begin PaneConfiguration = 9
+         NumPanes = 2
+         Configuration = "(H (1 [75] 4))"
+      End
+      Begin PaneConfiguration = 10
+         NumPanes = 2
+         Configuration = "(H (1[66] 2) )"
+      End
+      Begin PaneConfiguration = 11
+         NumPanes = 2
+         Configuration = "(H (4 [60] 2))"
+      End
+      Begin PaneConfiguration = 12
+         NumPanes = 1
+         Configuration = "(H (1) )"
+      End
+      Begin PaneConfiguration = 13
+         NumPanes = 1
+         Configuration = "(V (4))"
+      End
+      Begin PaneConfiguration = 14
+         NumPanes = 1
+         Configuration = "(V (2))"
+      End
+      ActivePaneConfig = 0
+   End
+   Begin DiagramPane = 
+      Begin Origin = 
+         Top = 0
+         Left = 0
+      End
+      Begin Tables = 
+         Begin Table = "m"
+            Begin Extent = 
+               Top = 6
+               Left = 38
+               Bottom = 102
+               Right = 224
+            End
+            DisplayFlags = 280
+            TopColumn = 0
+         End
+         Begin Table = "mo"
+            Begin Extent = 
+               Top = 6
+               Left = 262
+               Bottom = 136
+               Right = 448
+            End
+            DisplayFlags = 280
+            TopColumn = 0
+         End
+         Begin Table = "z"
+            Begin Extent = 
+               Top = 102
+               Left = 38
+               Bottom = 232
+               Right = 232
+            End
+            DisplayFlags = 280
+            TopColumn = 0
+         End
+      End
+   End
+   Begin SQLPane = 
+   End
+   Begin DataPane = 
+      Begin ParameterDefaults = ""
+      End
+   End
+   Begin CriteriaPane = 
+      Begin ColumnWidths = 12
+         Column = 1440
+         Alias = 900
+         Table = 1170
+         Output = 720
+         Append = 1400
+         NewValue = 1170
+         SortType = 1350
+         SortOrder = 1410
+         GroupBy = 1350
+         Filter = 1350
+         Or = 1350
+         Or = 1350
+         Or = 1350
+      End
+   End
+End
+', @level0type = N'SCHEMA', @level0name = N'rapOp', @level1type = N'VIEW', @level1name = N'v_ZamowieniaDlaMarek';
+
