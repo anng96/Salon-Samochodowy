d55e76467f8e6f6ca9eed0e7f7352e07a50d1699
anng96 <43572687+anng96@users.noreply.github.com>
2019-02-12 00:38:33 +0100

    Widok hr

---
+++ Baza salonu samochodowego/Baza salonu samochodowego/rapOp/Views/v_HrNajwyszaPremiaZ.sql
@@ -0,0 +1,9 @@
+﻿CREATE VIEW [rapOp].[v_HrNajwyszaPremiaZ] AS
+SELECT z.ZespolId, z.NazwaZespolu, p.PracownikId, p.Nazwisko, MAX(Kwota) as NajwyższaPremia
+FROM  [dbo].[Zespoly] AS z WITH (NOLOCK) 
+INNER JOIN Pracownicy as p on z.ZespolId=p.ZespolId
+INNER JOIN [dbo].[Wyplaty] AS w ON p.PracownikId = w.PracownikId
+WHERE Typ='premia'
+GROUP BY z.ZespolId, z.NazwaZespolu, p.PracownikId, p.Nazwisko
+
+--WIDOK ZWRACAJĄCY ID ZESPOŁU, NAZWĘ, ID PRACOWNIKA I NAZWISKO, O NAJWYŻSZEJ PREMI W ZESPOLE
