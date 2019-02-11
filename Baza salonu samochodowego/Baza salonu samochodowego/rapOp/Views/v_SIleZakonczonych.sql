f267384b029ea4b059211bbae1f7b0d5e1f51adc
anng96 <43572687+anng96@users.noreply.github.com>
2019-02-12 00:42:24 +0100

    poprawiony widok serwis

---
+++ Baza salonu samochodowego/Baza salonu samochodowego/rapOp/Views/v_SIleZakonczonych.sql
@@ -0,0 +1,7 @@
+﻿CREATE VIEW [rapOp].[v_SIleZakonczonych] AS
+SELECT COUNT (StatusId) AS IleZamknietych 
+FROM StatusZamowienia AS s WITH (NOLOCK)
+INNER JOIN HistoriaZamowienia hs ON s.StatusId=hs.Status 
+WHERE JakiStatus = 'Zamknięte'
+
+--WIDOK ZWRACAJĄCY LICZBĘ ZAMÓWIEŃ KTÓRYCH AKTUALNY STATUS TO 'ZAMKNIĘTE'
