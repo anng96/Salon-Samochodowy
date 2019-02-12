501b20f4d1756069b9f37c697b39dcc374407fd7
anng96 <anngog96@gmail.com>
2019-02-12 10:03:58 +0100

    Delete v_SerwisIleZakonczonych.sql

--- Baza salonu samochodowego/Baza salonu samochodowego/rapOp/Views/v_SerwisIleZakonczonych.sql
+++
@@ -1,4 +0,0 @@
-﻿CREATE VIEW rapOp.v_SerwisIleZakonczonych AS
-SELECT COUNT (StatusId) AS IleZamknietych 
-FROM StatusZamowienia AS s INNER JOIN HistoriaZamowienia hs ON s.StatusId=hs.Status 
-WHERE JakiStatus = 'Zamknięte'
